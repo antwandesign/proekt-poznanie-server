3bea738eb64015d2fb46f1b1f2b16da639536909
antwandesign <39638303+antwandesign@users.noreply.github.com>
2022-08-22 21:08:13 +0300

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('me')
+  @UseGuards(JwtGuard)
+  getMe(@GetUser() user: User) {
+    return { msg: user };
+  }
+}
