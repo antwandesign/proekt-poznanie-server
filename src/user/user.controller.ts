fe03e4fa72ec96c14d427b4e56aada2d2b207f3f
antwandesign <39638303+antwandesign@users.noreply.github.com>
2022-08-23 13:12:33 +0300

    Cleanup and commenting

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
@@ -7,10 +13,20 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
-
+  //Get details about "me"
+  //Route guarded by JwtGuard
+  //Handled by @GetUser decorator in auth module
   @Get('me')
   @UseGuards(JwtGuard)
   getMe(@GetUser() user: User) {
-    return { msg: user };
+    return { user };
+  }
+
+  //Get details about user by id
+  //Route guarded by JwtGuard
+  @Get(':id')
+  @UseGuards(JwtGuard)
+  getAll(@Param('id', ParseIntPipe) id) {
+    return this.userService.getById(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  getById(id: number) {
+    return this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
